b17a2ac67238943264c9afa37e6844ece04668db
Jeff-Adler <Jeffm.adler@gmail.com>
2021-06-28 20:47:55 -0400

    created reducer

---
+++ src/state/reducers/repositoriesReducer.ts
@@ -0,0 +1,17 @@
+interface RepositoriesState {
+  loading: boolean;
+  error: string | null;
+  data: string[];
+}
+
+const reducer = (state: RepositoriesState, action: any) => {
+  switch (action.type) {
+    case 'search_repositories':
+    case 'search_repositories_success':
+    case 'search_repositories_error':
+    default:
+      return state;
+  }
+};
+
+export default reducer;
