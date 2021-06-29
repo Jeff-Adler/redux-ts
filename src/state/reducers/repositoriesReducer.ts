ae5abacaf13c4ee1421868d1ba46da25b9e8065d
Jeff-Adler <Jeffm.adler@gmail.com>
2021-06-28 21:26:13 -0400

    added search repositories enum

--- src/state/reducers/repositoriesReducer.ts
+++ src/state/reducers/repositoriesReducer.ts
@@ -18,26 +18,33 @@ interface SearchRepositoriesErrorAction {
   payload: string;
 }
 
-const reducer = (
-  state: RepositoriesState,
-  action: SearchRepositoriesAction | SearchRepositoriesSuccessAction | SearchRepositoriesErrorAction
-): RepositoriesState => {
+type Action = SearchRepositoriesAction | SearchRepositoriesSuccessAction | SearchRepositoriesErrorAction;
+
+enum ActionType {
+  SEARCH_REPOSITORIES = 'search_repositories',
+  SEARCH_REPOSITORIES_SUCCESS = 'search_repositories_success',
+  SEARCH_REPOSITORIES_ERROR = 'search_repositories_error',
+}
+
+const reducer = (state: RepositoriesState, action: Action): RepositoriesState => {
   // action.type is read from the interfaces associated with action argument. So, it knows what payload to expect for every possible type
   switch (action.type) {
     // case statements function as type guards: if a case evaluates to T, the associated action is fully determined
-    case 'search_repositories':
+    case ActionType.SEARCH_REPOSITORIES:
       return {
         loading: true,
         error: null,
         data: [],
       };
-    case 'search_repositories_success':
+    case ActionType.SEARCH_REPOSITORIES_SUCCESS:
       return {
         loading: false,
+        // TS knows this should throw error, cause type of action.payload for this action doesn't match IRepositoriesState
+        // error: action.payload,
         error: null,
         data: action.payload,
       };
-    case 'search_repositories_error':
+    case ActionType.SEARCH_REPOSITORIES_ERROR:
       return {
         loading: false,
         error: action.payload,
